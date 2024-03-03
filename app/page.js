2e74d4a4bbceb7771abdf2d262e9f18c38c5d289
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2024-03-03 13:08:38 -0600

    added HeroSection

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <h1 className="text-white">Portfolio Website</h1>
+    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
+      <HeroSection />
     </main>
   );
 }
