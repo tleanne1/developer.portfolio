5ab55d04447c434c346e03fa48f26c1456f1dd26
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2024-03-06 22:00:46 -0600

    Updated EmailSection

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,22 +1,26 @@
-// import { EmailTemplate } from "../../../components/EmailTemplate";
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST() {
+export async function POST(req, res) {
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
-      from: "Tracey <tracey@tleanne.dev>",
-      to: ["tracey@tleanne.dev"],
-      subject: "Hello world",
+      from: fromEmail,
+      to: [fromEmail, email],
+      subject: subject,
       react: (
         <>
-          <p>Email Body</p>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting us!</p>
+          <p>New message submitted:</p>
+          <p>{message}</p>
         </>
       ),
     });
-
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json({ error });
--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Navbar from "@/components/Navbar";
 import AboutSection from "@/components/AboutSection";
 import ProjectSection from "@/components/ProjectSection";
 import EmailSection from "@/components/EmailSection";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <ProjectSection />
         <EmailSection />
       </div>
+      <Footer />
     </main>
   );
 }
